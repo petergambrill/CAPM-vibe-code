a500251f8cf9bd1ca4ee9d59f7d805156a58dc68
Peter Gambrill <petergambrill@gmail.com>
2025-08-09 17:44:15 +0100

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "UKRN CAPM App" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen bg-white">{children}</body>
-    </html>
-  );
-}
